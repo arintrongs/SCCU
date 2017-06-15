07425c4148fe2509cc9f83ce5fc5002a3405e06b
l3lackclevil <l3lackclevil@hotmail.com>
2017-06-15 22:43:32 +0700

    First Commit

---
+++ js/newsTicker.js
@@ -0,0 +1,7 @@
+var nt_example1 = $('#nt-example1').newsTicker({
+    row_height: 80,
+    max_rows: 3,
+    duration: 4000,
+    prevButton: $('#nt-example1-prev'),
+    nextButton: $('#nt-example1-next')
+});
